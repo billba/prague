0d2220c585025a909fb8433429da9b1018ae8cc5
Bill Barnes <bill@billba-mbp.local>
2017-04-01 16:50:43 +0200

    Simple browser bot, web pack

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,25 @@
-console.log("Hey it compiled");
+import { Observable, BehaviorSubject } from 'rxjs';
+import { IBotConnection, Activity, ConnectionStatus } from 'botframework-directlinejs';
+
+class BrowserBot implements IBotConnection {
+    constructor() {
+    }
+
+    public postActivity(activity: Activity): Observable<string> {
+        return Observable.of("success");
+    }
+
+    public activity$ = Observable.timer(1000,3000).map(i => ({
+        type: 'message',
+        id: i.toString(),
+        timestamp: (new Date()).toISOString(),
+        from: { id: 'browserBot'},
+        text: `hello, world #${i}`
+    } as Activity));
+
+    public connectionStatus$ = new BehaviorSubject(ConnectionStatus.Online);
+    public end() {
+    }
+}
+console.log("setting browserBot");
+window["browserBot"] = new BrowserBot();
