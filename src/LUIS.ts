58374193c2b638082d87309bcd4d781608893d3c
Bill Barnes <bill@billba-mbp.local>
2017-04-18 17:02:18 -0700

    Halfway through LUIS update

--- src/LUIS.ts
+++ src/LUIS.ts
@@ -1,53 +1,41 @@
 import { Observable } from 'rxjs';
-import { ITextInput, Action, Rule, bestMatch$, Match } from './Rules';
+import { ITextInput, Action, Rule, bestMatch$, Match, observize, Observizeable } from './Rules';
 
 // a temporary model for LUIS built from my imagination because I was offline at the time
 
-interface LuisMatch {
-    intent: string,
-    entities: any,
-    score: number,
+export interface LuisIntent {
+    intent: string;
+    score: number;
 }
 
-interface LuisResponse {
-    matchers: LuisMatch[];
+export interface LuisEntity {
+    entity: string;
+    type: "accountName";
+    startIndex: number;
+    endIndex: number;
+    score: number;
+}
+
+export interface LuisResponse {
+    query: string;
+    topScoringIntent: LuisIntent;
+    intents: LuisIntent[];
+    entities: LuisEntity[];
 }
 
 interface LuisCache {
-    [message: string]: LuisMatch[];
-}
-
-export interface LuisCredentials {
-    name: string,
-    id: string,
-    key: string,
-}
-
-interface LuisModel extends LuisCredentials {
-    results: LuisCache
-}
-
-interface LuisModels {
-    [name: string]: LuisModel
+    [utterance: string]: LuisResponse;
 }
 
 export interface LuisRule<S extends ITextInput> {
-    intent: string,
-    action: Action<S>,
+    intent: string;
+    action: Action<S>;
 }
 
 export class LUIS<S extends ITextInput> {
-    private models: LuisModels = {};
+    private cache: LuisCache = {};
 
-    constructor(... creds: LuisCredentials[]) {
-        creds.forEach(cred => this.addModel(cred))
-    }
-
-    addModel(creds: LuisCredentials) {
-        this.models[creds.name] = {
-            ... creds,
-            results: {}
-        }
+    constructor(private id: string, private key: string, private scoreThreshold = 0.5) {
     }
 
     private testData = {
@@ -98,22 +86,16 @@ export class LUIS<S extends ITextInput> {
     }
 
     // a mock because I don't really care about really calling LUIS yet
-    private call(modelName: string, utterance: string): Observable<LuisMatch[]> {
-        const model = this.models[modelName];
-        if (!model) {
-            console.error(`no LUIS model with name ${modelName} - provide with constructor or call addModel`)
-            return Observable.empty();
-        }
-
-        let result = model.results[utterance];
-        if (!result) {
-            result = this.testData[utterance];
-            if (!result)
+    private call(utterance: string): Observable<LuisResponse> {
+        let response = this.cache[utterance];
+        if (!response) {
+            response = this.testData[utterance]; // this emulates the call
+            if (!response)
                 return Observable.empty();
-            model.results[utterance] = result;
+            this.cache[utterance] = response;
         }
 
-        return Observable.of(result);
+        return Observable.of(response);
     }
 
     intent(intent: string, action: Action<S>): LuisRule<S> {
@@ -123,27 +105,57 @@ export class LUIS<S extends ITextInput> {
         }
     }
 
+    raw(action: (input: S, luisResponse: LuisResponse) => Observizeable<any>): Rule<S> {
+        return (input) =>
+            this.call(input.text)
+            .map(luisResponse => {
+                action: () => action(input, luisResponse)
+            });
+    }
+
     // "classic" LUIS usage - for a given model, say what to do with each intent above a given threshold
     // IMPORTANT: the order of rules is not important - the action for the *highest-ranked intent* will be executed
-    bestMatch(modelName: string, luisRules: LuisRule<S>[], threshold = .50): Rule<S> {
-        return (input) =>
-            this.call(modelName, input.text)
-            .flatMap(luisResult =>
-                Observable.from(luisResult)
-                .do(luisMatch => console.log("luisMatch", luisMatch))
-                .filter(luisMatch => luisMatch.score >= threshold)
-                .flatMap(luisMatch =>
-                    Observable.of(luisRules.find(luisRule => luisRule.intent === luisMatch.intent))
-                    .filter(luisRule => !!luisRule)
-                    .do(_ => console.log("filtered luisMatch", luisMatch))
-                    .map(luisRule => ({
-                        score: luisMatch.score,
-                        action: () => luisRule.action(input, luisMatch.entities)
-                    } as Match))
-                )
-                .take(1) // LUIS returns results ordered by best match, we return the first in our list over our threshold
-            );
+    bestMatch(luisRules: LuisRule<S>[]): Rule<S> {
+        return this.raw((input: S, args: LuisResponse) =>
+            Observable.from(luisResult.intents)
+            .do(intent => console.log("intent", intent))
+            .filter(intent => intent.score >= this.scoreThreshold)
+            .flatMap(intent =>
+                Observable.of(luisRules.find(luisRule => luisRule.intent === intent.intent))
+                .filter(luisRule => !!luisRule)
+                .do(_ => console.log("filtered intent", intent))
+                .map(luisRule => ({
+                    score: intent.score,
+                    action: () => luisRule.action(input, luisResult.entities)
+                } as Match))
+            )
+            .take(1) // LUIS returns results ordered by best match, we return the first in our list over our threshold
+        );
     }
+
+
+
+    azureCheckValue = (action: (input: S, args: any) => any) => (input: S) =>
+        luis.raw((input: S, args: LuisResponse) => )
+    // customMatch(modelName: string, matcher: (input: S, luisMatches: LuisMatch[]) => Observizeable<Match>): Rule<S> {
+    //     return (input) =>
+    //         this.call(modelName, input.text)
+    //         .flatMap(luisResult => observize(action(input, luisResult))
+    //             Observable.from(luisResult)
+    //             .do(luisMatch => console.log("luisMatch", luisMatch))
+    //             .filter(luisMatch => luisMatch.score >= threshold)
+    //             .flatMap(luisMatch =>
+    //                 Observable.of(luisRules.find(luisRule => luisRule.intent === luisMatch.intent))
+    //                 .filter(luisRule => !!luisRule)
+    //                 .do(_ => console.log("filtered luisMatch", luisMatch))
+    //                 .map(luisRule => ({
+    //                     score: luisMatch.score,
+    //                     action: () => luisRule.action(input, luisMatch.entities)
+    //                 } as Match))
+    //             )
+    //             .take(1) // LUIS returns results ordered by best match, we return the first in our list over our threshold
+    //         );
+    // }
 
 }
 
