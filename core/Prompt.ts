bea959ac7194866485f638cbad664e3f5b59362b
Bill Barnes <bill@billba-mbp.local>
2017-06-20 08:34:51 -0700

    Prompts -> Dialogs

--- core/Prompt.ts
+++ core/Prompt.ts
@@ -1,43 +1,7 @@
 import { konsole } from './Konsole';
-import { IRule, SimpleRule, Observableable, RuleResult, BaseRule, Matcher, Handler, Match, combineMatchers } from './Rules';
+import { IRule, Matcher, Match } from './Rules';
 import { Observable } from 'rxjs';
 import { ITextMatch } from './Text';
-
-export interface PromptRules<M> {
-    [promptKey: string]: IRule<M>;
-}
-
-export class Prompts<M extends Match = any> extends BaseRule<M> {
-    constructor(
-        private rules: PromptRules<M>,
-        private getPromptKey: (match: M) => string,
-        private setPromptKey: (match: M, promptKey?: string) => void
-    ) {
-        super();
-    }
-
-    tryMatch(match: M): Observable<RuleResult> {
-        return Observable.of(this.getPromptKey(match))
-            .do(promptKey => konsole.log("promptKey", promptKey))
-            .filter(promptKey => promptKey !== undefined)
-            .map(promptKey => this.rules[promptKey])
-            .filter(rule => rule !== undefined)
-            .flatMap(rule =>
-                rule.tryMatch(match)
-                .map(ruleResult => ({
-                    ... ruleResult,
-                    action: () => {
-                        this.setPromptKey(match, undefined);
-                        return ruleResult.action();
-                    }
-                }))
-            );
-    }
-
-    setPrompt(match, promptKey: string) {
-        this.setPromptKey(match, promptKey);
-    }
-}
 
 export interface IChatPromptConfirmMatch {
     confirm: boolean,
@@ -47,34 +11,21 @@ export interface IChatPromptChoiceMatch {
     choice: string,
 }
 
-export class TextPrompts<M extends Match & ITextMatch> extends Prompts<M> {
-    matchChoice(choices: string[]): Matcher<M, M & IChatPromptChoiceMatch> {
-        return match =>
-            Observable.of(choices.find(choice => choice.toLowerCase() === match.text.toLowerCase()))
-            .filter(choice => !!choice)
-            .map(choice => ({
-                ... match as any, // remove "as any" when TypeScript fixes this bug
-                choice
-            }));
-    }
-
-    matchConfirm(): Matcher<M & IChatPromptChoiceMatch, M & IChatPromptConfirmMatch> {
-        return match => ({
+export const matchChoice = (choices: string[]) =>
+    <M extends Match & ITextMatch = any>(match: M) => {
+        const choice = choices.find(choice => choice.toLowerCase() === match.text.toLowerCase());
+        return choice && {
             ... match as any, // remove "as any" when TypeScript fixes this bug
-            confirm: match.choice === 'Yes' 
-        });
+            choice
+        } as M & IChatPromptChoiceMatch
     }
 
-    choice(choices: string[], handler: Handler<M & IChatPromptChoiceMatch>): IRule<M> {
-        return new SimpleRule<M>(this.matchChoice(choices), handler);
+export const matchConfirm = () =>
+    <M extends Match & ITextMatch = any>(match: M) => {
+        const m = matchChoice(['Yes', 'No'])(match);
+        return m && {
+            ... match as any, // remove "as any" when TypeScript fixes this bug
+            confirm: m.choice === 'Yes' 
+        } as M & IChatPromptConfirmMatch;
     }
 
-    confirm(handler: Handler<M & IChatPromptConfirmMatch>): IRule<M> {
-        const choices = ['Yes', 'No'];
-        return new SimpleRule<M>(
-            this.matchChoice(choices),
-            this.matchConfirm(),
-            handler
-        );
-    }
-}
