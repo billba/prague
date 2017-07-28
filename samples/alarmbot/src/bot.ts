53edbee807ff327fdf8d197d76de34bb80532b1a
Bill Barnes <bill@billba-mbp.local>
2017-07-28 16:40:36 -0700

    use latest Prague

--- samples/alarmbot/src/bot.ts
+++ samples/alarmbot/src/bot.ts
@@ -3,7 +3,7 @@ import { BrowserBot } from 'prague-botframework-browserbot';
 
 const webChat = new WebChatConnector()
 window["browserBot"] = webChat.botConnection;
-const browserBot = new BrowserBot(new UniversalChat(webChat.chatConnector), undefined);
+const browserBot = new BrowserBot(new UniversalChat(webChat.chatConnector), {});
 
 // This is our "base message type" which is used often enough that we made it really short
 
@@ -39,50 +39,95 @@ const dialogs = new Dialogs<B>({
     } 
 );
 
+import { IActivityMatch } from 'prague-botframework';
+
+const routingEvent = <M extends IActivityMatch = any>(m: M) => ({
+    type: 'event',
+    name: 'routing',
+    from: { id: m.activity.from.id },
+    conversationId: { id: m.activity.conversation.id },
+    channelId: m.activity.channelId
+});
+
+const postMessage = <M extends object = any>(m: M) => {
+    // post message here
+}
+
+const postRoutingEvent = <M extends IActivityMatch = any>(m: M) =>
+    postMessage(routingEvent(m));
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 interface AlarmState {
-    title: string,
-    time: Date
+    title?: string,
+    time?: Date
 }
 
 const setAlarm = (alarmState: AlarmState) => {
     // set alarm here
 }
 
+const titleDialog = dialogs.add<{}, { title: string } >(
+    'getTitle',
+    (dialog, m) =>
+        m.reply("What shall I call the alarm?"),
+    (dialog) => first(
+        m => dialog.end({ title: m.text })
+    )
+)
+
+const timeDialog = dialogs.add<{}, { time: string } >(
+    'getTime',
+    (dialog, m) =>
+        m.reply("For when shall I set the alarm?"),
+    (dialog) => first(
+        m => dialog.end({ time: m.text })
+    )
+)
+
 const setAlarmDialog = dialogs.add<AlarmState, AlarmState, AlarmState>(
     'setAlarm',
-    (dialog, m) => m.reply("Let's set an alarm. What do you want to call it?"),
+    (dialog, m) => {
+        dialog.state = dialog.args;
+        m.reply("Let's set an alarm.");
+        return dialog.routeMessage(m);
+    },
     (dialog) => first(
-        ifMatchRE(/help/, m => {}),
-        ifMatch(_ => !dialog.state.title, m => {
-            dialog.state.title = m.text;
-            m.reply("For when shall I set the alarm?");
+        dialog.routeTo(titleDialog, _ => !dialog.state.title, {}, m => {
+            dialog.state.title = m.dialogResponse.title;
+            // return postMessage(m);
         }),
-        ifMatch(_ => !dialog.state.time, m => {
-            dialog.state.time = new Date(m.text);
+        dialog.routeTo(timeDialog, _ => !dialog.state.time, {}, m => {
+            dialog.state.time = new Date(m.dialogResponse.time);
+            // return postMessage(m);
+        }),
+        m => {
             setAlarm(dialog.state);
             m.reply(`Great, I set an alarm called ${dialog.state.title} for ${dialog.state.time.toString()}.`);
             return dialog.end();
-        })
+        }
     )
-)
+);
 
 const rootDialog = dialogs.add(
     'root',
-    (dialog) => dialog.first(
-        ifMatchRE(/set alarm/i, m => dialog.activate(setAlarmDialog)),
-        ifMatchRE(/delete alarm/i, m => m.reply("let's delete an alarm")),
-        ifMatchRE(/list alarms/i, m => m.reply("let's list the alarms")),
-        m => m.reply("Hi... I'm the alarm bot sample. I can set new alarms or delete existing ones.")
+    (dialog) => first(
+        dialog.routeTo(setAlarmDialog, matchRE(/set (?:an ){0,1}alarm(?: (?:named|called )(.*)){0,1}/), m => ({ title: m.groups[1] } as AlarmState)),
+        // dialog.routeTo('deleteAlarm', matchRE(/delete alarm/i)),
+        // dialog.routeTo('listAlarms', matchRE(/list alarms/i)),
+        ifMatchRE(/help/, m => m.reply("global help")),
+        m => m.reply("Hi... I'm the alarm bot sample. I can set new alarms, delete existing ones, and list the ones you have.")
     )
 )
 
+const appRouter: IRouter<B> = dialogs.routeToRoot('root');
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const appRouter: IRouter<B> = dialogs.routeToRoot('root');
+import { Subject } from 'rxjs';
 
 browserBot.run({
     message: appRouter,
 });
+
 
