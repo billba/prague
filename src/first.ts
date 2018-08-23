0e0b3c6b83c354c91f497a457c6d04307ca220ca
Bill Barnes <billba@microsoft.com>
2018-08-23 13:29:33 -0700

    first no longer takes null | undefined transforms

--- src/first.ts
+++ src/first.ts
@@ -3,79 +3,71 @@ import { from as observableFrom} from "rxjs";
 import { concatMap, take } from "rxjs/operators";
 import { transformToNull } from "./core";
 
-export function first(): Transform<[], null>;
-
 export function first <
-    ARGS extends any[] = [],
-    R0 = never,
+    ARGS extends any[],
+    R0,
 > (...args: [
-    null | undefined | ((...args: ARGS) => R0)
+    (...args: ARGS) => R0
 ]): Transform<ARGS, Norm<R0>>;
 
 export function first <
-    ARGS extends any[] = [],
-    R0 = never,
-    R1 = never,
+    ARGS extends any[],
+    R0,
+    R1,
 > (...args: [
-    null | undefined | ((...args: ARGS) => R0),
-    null | undefined | ((...args: ARGS) => R1)
+    (...args: ARGS) => R0,
+    (...args: ARGS) => R1
 ]): Transform<ARGS, Norm<NonNullable<R0> | R1>>;
 
 export function first <
-    ARGS extends any[] = [],
-    R0 = never,
-    R1 = never,
-    R2 = never,
+    ARGS extends any[],
+    R0,
+    R1,
+    R2,
 > (...args: [
-    null | undefined | ((...args: ARGS) => R0),
-    null | undefined | ((...args: ARGS) => R1),
-    null | undefined | ((...args: ARGS) => R2)
+    (...args: ARGS) => R0,
+    (...args: ARGS) => R1,
+    (...args: ARGS) => R2
 ]): Transform<ARGS, Norm<NonNullable<R0 | R1> | R2>>;
 
 export function first <
-    ARGS extends any[] = [],
-    R0 = never,
-    R1 = never,
-    R2 = never,
-    R3 = never,
+    ARGS extends any[],
+    R0,
+    R1,
+    R2,
+    R3,
 > (...args: [
-    null | undefined | ((...args: ARGS) => R0),
-    null | undefined | ((...args: ARGS) => R1),
-    null | undefined | ((...args: ARGS) => R2),
-    null | undefined | ((...args: ARGS) => R3)
+    (...args: ARGS) => R0,
+    (...args: ARGS) => R1,
+    (...args: ARGS) => R2,
+    (...args: ARGS) => R3
 ]): Transform<ARGS, Norm<NonNullable<R0 | R1 | R2> | R3>>;
 
 export function first <
-    ARGS extends any[] = [],
-    R0 = never,
-    R1 = never,
-    R2 = never,
-    R3 = never,
-    R4 = never,
+    ARGS extends any[],
+    R0,
+    R1,
+    R2,
+    R3,
+    R4,
 > (...args: [
-    null | undefined | ((...args: ARGS) => R0),
-    null | undefined | ((...args: ARGS) => R1),
-    null | undefined | ((...args: ARGS) => R2),
-    null | undefined | ((...args: ARGS) => R3),
-    null | undefined | ((...args: ARGS) => R4)
+    (...args: ARGS) => R0,
+    (...args: ARGS) => R1,
+    (...args: ARGS) => R2,
+    (...args: ARGS) => R3,
+    (...args: ARGS) => R4
 ]): Transform<ARGS, Norm<NonNullable<R0 | R1 | R2 | R3> | R4>>;
 
 export function first <
-    ARGS extends any[] = [],
+    ARGS extends any[],
 > (...args:
-    (null | undefined | ((...args: ARGS) => any))[]
+    ((...args: ARGS) => any)[]
 ): Transform<ARGS, Result>;
 
 export function first (
-    ...transforms: (null | undefined | ((...args: any[]) => any))[]
+    ...transforms: ((...args: any[]) => any)[]
 ) {
-    if (transforms.length === 0)
-        return transformToNull;
-
-    const _transforms = observableFrom(transforms
-        .filter(transform => !!transform)
-        .map(transform => from(transform))
-    );
+    const _transforms = observableFrom(transforms.map(transform => from(transform)));
 
     return from((...args: any[]) => _transforms.pipe(
         // we put concatMap here because it forces everything to after it to execute serially
