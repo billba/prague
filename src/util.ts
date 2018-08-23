4544fe9deb5b9a852bd976180d7fdc156fa67e74
Bill Barnes <billba@microsoft.com>
2018-08-23 14:22:59 -0700

    + matchRE, ifMatchRE

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,22 @@
+import { match, Value, matchIf } from './prague';
+
 export const re = (regexp: RegExp) => (text: string) => regexp.exec(text);
+
+export const matchRE = <
+    ONMATCH,
+    ONNOMATCH = null,
+> (
+    regexp: RegExp,
+    onMatch: (value: Value<RegExpExecArray>) => ONMATCH,
+    onNoMatch?: () => ONNOMATCH,
+) => match(re(regexp), onMatch, onNoMatch);
+
+
+export const ifMatchRE = <
+    ONMATCH,
+    ONNOMATCH = null,
+> (
+    regexp: RegExp,
+    onMatch: () => ONMATCH,
+    onNoMatch?: () => ONNOMATCH,
+) => match(re(regexp), onMatch, onNoMatch);
