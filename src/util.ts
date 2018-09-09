15168b0e690fb3fafaf9b316e9f728f6ba3633d3
Bill Barnes <billba@microsoft.com>
2018-09-10 05:00:56 +0800

    + getFetchJson

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,16 @@
 export const re = (regexp: RegExp) => (text: string) => regexp.exec(text);
+
+export const getFetchJson = (
+    error: string | ((body: string) => string)
+) => (r: Response) => {
+    if (!r.ok) {
+        if (typeof error === 'string')
+            throw error;
+        
+        return r.text().then(body => {
+            throw error(body);
+        });
+    }
+
+    return r.json();
+}
